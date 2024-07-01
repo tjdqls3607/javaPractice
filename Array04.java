cf530664ed16759270cc638cf6bd3e19bf4de4ba
정성빈 <tjdqls3124@gmail.com>
2024-07-01 11:33:32 +0900

    Add files via upload

---
+++ Array04.java
@@ -0,0 +1,20 @@
+package H;
+
+public class Array04 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] scores = {83, 90, 87};
+		
+		int sum = 0;
+		for(int i=0; i<scores.length; i++) {
+			sum += scores[i];
+		}
+		
+		System.out.println("총합 : " + sum);
+		
+		double avg = (double) sum/ scores.length;
+		System.out.println("평균 :" + avg);
+	}
+
+}
