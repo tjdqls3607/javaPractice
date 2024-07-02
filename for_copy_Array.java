965bb40071d618c40e076ba095caf13ab9bda91f
Jeong Seong Bin <tjdqls3124@gmail.com>
2024-07-02 10:32:40 +0900

    for문으로 배열 복사하기

---
+++ for_copy_Array.java
@@ -0,0 +1,20 @@
+package H;
+
+public class for_copy_Array {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[]oldIntArray = {1,2,3};
+		int[]newIntArray = new int[5];
+		
+		
+		for(int i=0; i<oldIntArray.length; i++) {
+			newIntArray[i] = oldIntArray[i];
+		}
+		
+		for(int i=0; i<newIntArray.length; i++) {
+			System.out.print(newIntArray[i] + ", ");
+		}
+	}
+
+}
