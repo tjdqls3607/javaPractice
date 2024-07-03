14bc540ba060fd13f779bf5d82a4b6bf41623413
Jeong Seong Bin <tjdqls3124@gmail.com>
2024-07-03 10:28:38 +0900

    객체생성 생성자 예제소스코드

---
+++ Korean.java
@@ -0,0 +1,14 @@
+package H;
+
+public class Korean {
+	// TODO Auto-generated method stub
+	String nation = "대한민국";
+	String name;
+	String ssn;
+		
+		//생성자
+		public Korean(String n, String s) {
+			name = n;
+			ssn = s;
+		}
+}
---
+++ KoreanExample01.java
@@ -0,0 +1,17 @@
+package H;
+
+public class KoreanExample01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Korean k1 = new Korean("박자바", "011225-1234567");
+		System.out.println("K1.name: " + k1.name);
+		System.out.println("K1.ssn: " + k1.ssn);
+		
+		Korean k2 = new Korean("김자바", "930525-0654321");
+		System.out.println("K.2name: " + k2.name);
+		System.out.println("K2.ssn: " + k2.ssn);
+
+	}
+
+}
