9515b2560da7271f0cdf92d426845304e149a980
Jeong Seong Bin <tjdqls3124@gmail.com>
2024-07-04 10:25:59 +0900

    인텔리제이 클래스내부에서 메소드 호출 및 execute 실행

---
+++ Cal.java
@@ -0,0 +1,23 @@
+package H;
+
+public class Cal {
+    int plus(int x, int y) {
+        int result = x + y;
+        return result;
+    }
+
+    double avg(int x, int y) {
+        double sum = plus(x, y);
+        double result = sum / 2;
+        return result;
+    }
+
+    void execute() {
+        double result = avg(7, 10);
+        println("실행 결과: " + result);
+    }
+
+    void println(String massage) {
+        System.out.println(massage);
+    }
+}
---
+++ CalExample.java
@@ -0,0 +1,8 @@
+package H;
+
+public class CalExample {
+    public static void main(String[] args) {
+        Cal myCal = new Cal ();
+        myCal.execute();
+    }
+}
