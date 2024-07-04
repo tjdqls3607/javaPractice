f9ccce3f70bc4f05829898c31425478256e43c48
Jeong Seong Bin <tjdqls3124@gmail.com>
2024-07-04 11:26:35 +0900

    메소드 오버로딩 정사각형 직사각형 넓이 구하는 예제소스

---
+++ square.java
@@ -0,0 +1,13 @@
+package H;
+
+public class square {
+    //정사각형의 넓이
+    double areaRectangle(double width) {
+        return width * width;
+    }
+
+    //직사각형의 넓이
+    double areaRectangle(double width, double height) {
+        return width * height;
+    }
+}
