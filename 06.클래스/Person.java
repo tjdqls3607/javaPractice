7d90ef9ca65fa64d90d11a44d21139a70fce2cf2
Jeong Seong Bin <tjdqls3124@gmail.com>
2024-07-05 11:53:57 +0900

    final 필드 선언

---
+++ 06.클래스/Person.java
@@ -0,0 +1,13 @@
+package H;
+
+public class Person {
+    final String nation = "Korea";
+    final String ssn;
+    String name;
+
+    public Person(String ssn, String name) {
+        this.ssn = ssn;
+        this.name = name;
+    }
+
+}
