08224b8f1a43b28c2571cc2a7688689e3f8b6c46
Jeong Seong Bin <tjdqls3124@gmail.com>
2024-08-13 14:45:16 +0900

    Create math.java

---
+++ Math클래스/math.java
@@ -0,0 +1,9 @@
+import java.util.Random;
+
+public class MathExam{
+    public static void main(String[] args){
+        System.out.println("2의 10승 = " + Math.pow(2, 10));
+        System.out.println("16의 1/2승 = : " + Math.pow(16, 0.5));
+        System.out.println("log200 = " + Math.log10(200));
+    }
+}
