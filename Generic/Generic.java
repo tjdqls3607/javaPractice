88bc9a98f4fe7610bf56342653e0f62cdb2e1df8
Jeong Seong Bin <tjdqls3124@gmail.com>
2024-08-13 15:32:26 +0900

    Create Generic.java

---
+++ Generic/Generic.java
@@ -0,0 +1,10 @@
+public class Box<E> {
+        private E obj;
+        public void setObj(E obj){
+            this.obj = obj;
+        }
+
+        public E getObj(){
+            return obj;
+        }
+    }
